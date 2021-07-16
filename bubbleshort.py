6d23f6fd82040f77485f55ce60238adbea5474f5
Anna Marchia <72507519+AnnaMarchia01@users.noreply.github.com>
2021-07-16 19:55:36 +0700

    Add files via upload

---
+++ bubbleshort.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def bubbleSort(mylist):
+    # outer loop
+    for i in range(len(mylist)-1, 0, -1):
+        # inner loop
+        for j in range(i):
+            if mylist[j] > mylist[j+1]:
+                # swap the value
+                temp = mylist[j]
+                mylist[j] = mylist[j+1]
+                mylist[j+1] = temp
+                
+    return mylist
+
+firstlist = [5, 8, 3, 1]
+print(bubbleSort(firstlist))
+
+# RESULT
+# [1, 3, 5, 8]
